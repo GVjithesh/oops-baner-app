98eb103a6698ab5e070c3febb52718ed022468ca
GVjithesh <gvjithesh@gmail.com>
2026-02-16 14:43:32 +0530

    UC2-BannerOOPS

---
+++ UC2OOPS.java
@@ -0,0 +1,13 @@
+/*UC2 - BannerOOPS*/
+public class UC1OOPS{
+ public static void main(String args[]){
+     System.out.println("  ***   " + "  ***   " + "  ***** " + "  ***** ");
+     System.out.println(" ** **  " + " ** **  " + "  **  **" + "  **    ");
+     System.out.println("**   ** " + "**   ** " + "  **  **" + "  **    ");
+     System.out.println("**   ** " + "**   ** " + "  ***** " + "  ***** ");
+     System.out.println("**   ** " + "**   ** " + "  **    " + "      **");
+     System.out.println(" ** **  " + " ** **  " + "  **    " + "  **  **");
+     System.out.println("  ***   " + "  ***   " + "  **    " + "  ***** ");
+
+    }
+}
